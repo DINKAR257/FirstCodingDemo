a9ecc967d604bd123b1e6b040fdf732c8212b070
Vijay kumar <vdinkar400@gmail.com>
2022-12-09 08:24:46 +0530

    it is my first demo..

---
+++ src/Demo/demo_1.java
@@ -0,0 +1,17 @@
+package Demo;
+
+
+
+public class demo_1 {
+    public static void main(String[] args) {
+        System.out.println("1-Hello i m vijay kumar");
+        System.out.println("2-I m a B.com student...");
+        System.out.println("3-I done my graduation 2015.....");
+        System.out.println("4-I m a new learner for java coding etc....");
+        System.out.println("5-We use intellij && postman && oracle....");
+        System.out.println("And i learn more about coding with MOANJ bhati....");
+        System.out.println("I m learn coding at 2 months.....");
+        System.out.println("now we learn oracle.....");
+
+    }
+}
